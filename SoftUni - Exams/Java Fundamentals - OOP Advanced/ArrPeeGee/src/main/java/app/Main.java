ecf96a90630f932a5a3b6eb3d63f01483650ba7f
gpzhelyazkov <gpzhelyazkov@gmail.com>
2019-03-09 01:04:24 +0200

    Update

--- SoftUni - Exams/Java Fundamentals - OOP Advanced/ArrPeeGee/src/main/java/app/Main.java
+++ SoftUni - Exams/Java Fundamentals - OOP Advanced/ArrPeeGee/src/main/java/app/Main.java
@@ -20,35 +20,16 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) throws NoSuchMethodException, InstantiationException, IOException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
 
-        //Reader reader = new ConsoleReader();
-        //Writer writer = new ConsoleWriter();
+        Reader reader = new ConsoleReader();
+        Writer writer = new ConsoleWriter();
 
-        //ActionFactory actionFactory = new ActionFactoryImpl();
-        //TargetableFactory targetableFactory = new TargetableFactoryImpl();
-        //SpecialFactory specialFactory = new SpecialFactoryImpl();
+        ActionFactory actionFactory = new ActionFactoryImpl();
+        TargetableFactory targetableFactory = new TargetableFactoryImpl();
+        SpecialFactory specialFactory = new SpecialFactoryImpl();
 
-        //Battlefield battleField = new BattlefieldImpl(actionFactory, targetableFactory, specialFactory, writer);
-        //CommandHandler handler = new CommandHandlerImpl(battleField);
+        Battlefield battleField = new BattlefieldImpl(actionFactory, targetableFactory, specialFactory, writer);
+        CommandHandler handler = new CommandHandlerImpl(battleField);
 
-        //Engine engine = new GameEngine(reader, handler);
-        //engine.run();
-
-        int[] arr = {1, 1, 1, 2, 2, 5, 5, 3, 3, 4};
-
-        int i = 0;
-        int count = 0;
-        while (i + 1 < arr.length) {
-
-            if(arr[i] == arr[i + 1]) {
-                count++;
-                while (arr[i] == arr[i + 1]) {
-                    i++;
-                }
-            } else {
-                i++;
-        }
-    }
-
-        System.out.println(count);
-    }
+        Engine engine = new GameEngine(reader, handler);
+        engine.run();
 }
