4315b3a8e33616f447331c6a8a0a488db91e2c55
gpzhelyazkov <gpzhelyazkov@gmail.com>
2019-03-29 16:23:42 +0200

    New

--- SoftUni - Exams/Simple Task/Task/src/application/interfaces/Company.java
+++
@@ -1,9 +0,0 @@
-package application.interfaces;
-
-import application.EmployeeRecord;
-import java.util.List;
-
-public interface Company {
-
-    EmployeeRecord buildHierarchy(List<EmployeeRecord> employees);
-}
